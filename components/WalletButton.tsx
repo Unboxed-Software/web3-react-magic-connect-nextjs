7595368811173d78ec33520a171c92b8aa7dcbb8
John <johnzhengyuliu@gmail.com>
2023-03-20 02:17:23 -0500

    only show Magic Wallet button if connect with Magic

--- components/WalletButton.tsx
+++ components/WalletButton.tsx
@@ -1,25 +1,43 @@
 import { Button } from "@chakra-ui/react"
 import { MagicConnect } from "web3-react-magic"
 import { Connector } from "@web3-react/types"
+import { useEffect, useState } from "react"
 
-type WalletButtonProps = {
+interface WalletButtonProps {
   connector: MagicConnect | Connector
 }
 
 const WalletButton = ({ connector }: WalletButtonProps) => {
-  const handleOpenWallet = () => {
-    if (connector instanceof MagicConnect && connector.magic) {
-      connector.magic.wallet.getInfo().then((walletInfo) => {
-        if (walletInfo?.walletType == "magic") {
-          connector.magic.wallet.showUI().catch((err) => console.error(err))
-        }
-      })
+  const [showButton, setShowButton] = useState(false)
+
+  const handleOpenWallet = async () => {
+    if (showButton) {
+      try {
+        // @ts-ignore
+        await connector.magic.wallet.showUI()
+      } catch (error) {
+        console.error(error)
+      }
     }
   }
 
-  if (!(connector instanceof MagicConnect)) {
-    return null
-  }
+  useEffect(() => {
+    const checkWalletType = async () => {
+      const isMagicConnect =
+        connector instanceof MagicConnect && connector.magic
+
+      if (isMagicConnect) {
+        const walletInfo = await connector.magic.wallet.getInfo()
+        setShowButton(walletInfo?.walletType === "magic")
+      } else {
+        setShowButton(false)
+      }
+    }
+
+    checkWalletType()
+  }, [connector])
+
+  if (!showButton) return null
 
   return (
     <Button onClick={handleOpenWallet} w={100}>
